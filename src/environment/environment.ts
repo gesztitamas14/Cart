6c7d00b8ecef2e7a4c68010cdeac724f0fbea73a
h157909 <h157909@stud.u-szeged.hu>
2025-03-05 11:08:27 +0100

    Fix(.gitignore): Environment variables are now public

---
+++ src/environment/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    firebase: {apiKey: "AIzaSyAh00qesxvfoqyRiLgFrXhbebV9hHkwTEI",
+    authDomain: "cart-frederik.firebaseapp.com",
+    projectId: "cart-frederik",
+    storageBucket: "cart-frederik.firebasestorage.app",
+    messagingSenderId: "663935402544",
+    appId: "1:663935402544:web:00b7c832de70c729ada3f5"
+    },
+};
